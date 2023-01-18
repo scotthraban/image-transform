f0ec96bf5722995bc8c577eb1d7f35858fed8147
Scott Hraban <scotthraban@gmail.com>
2023-01-17 19:40:28 -0800

    change docker image version to 2.2

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ docker buildx build \
   --platform linux/arm64/v8,linux/amd64 \
   --push \
   --tag docker-registry.hraban.com/image-transform:latest \
-  --tag docker-registry.hraban.com/image-transform:2.1 \
+  --tag docker-registry.hraban.com/image-transform:2.2 \
   .
