19dfae5f5ed1f7508dbe52db1ed9006124cd2bd4
Scott Hraban <scotthraban@gmail.com>
2023-02-01 19:49:37 -0800

    change version to 3.0

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ docker buildx build \
   --platform linux/arm64/v8,linux/amd64 \
   --push \
   --tag docker-registry.hraban.com/image-transform:latest \
-  --tag docker-registry.hraban.com/image-transform:2.3 \
+  --tag docker-registry.hraban.com/image-transform:3.0 \
   .
